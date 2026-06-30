b642ce3a18c26974b529da6dcb00c46baedfe979
Erick <amisierick94@gmail.com>
2026-06-30 15:45:09 +0300

    Force template folder mapping

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
-# This explicitly maps the main domain link "/" to our admin page
 @app.route('/')
-def home():
+def index():
     return render_template('admin.html')
 
 if __name__ == '__main__':
