7e9216e66aaaf4d618e8b0c77a4ef37f3a8392f4
Erick <amisierick94@gmail.com>
2026-06-30 15:38:14 +0300

    Fixed routing map

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# This explicitly maps the main domain link "/" to our admin page
 @app.route('/')
 def home():
-    # This just displays our front-end page
     return render_template('admin.html')
 
 if __name__ == '__main__':
