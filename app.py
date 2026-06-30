9cc4f46849388c26763528e651e8f3dae5082a29
Erick <amisierick94@gmail.com>
2026-06-30 15:32:35 +0300

    Permanently cleaned and wiped old remote files

--- app.py
+++ app.py
@@ -1,82 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'library_clean_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-
-# =======================================================
-# DATABASE MODELS
-# =======================================================
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    role = db.Column(db.String(20), nullable=False) 
-
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    admission_no = db.Column(db.String(20), unique=True, nullable=False)
-    name = db.Column(db.String(100), nullable=False)
-    grade = db.Column(db.String(20), nullable=False) 
-
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    unique_book_id = db.Column(db.String(50), unique=True, nullable=False) 
-    title = db.Column(db.String(150), nullable=False)
-    author = db.Column(db.String(100), nullable=True)
-    status = db.Column(db.String(20), default='Available') 
-
-# =======================================================
-# ROUTES & WORKSPACE
-# =======================================================
-
-# THIS IS THE UPDATED CODE PLACED PERFECTLY HERE:
 @app.route('/')
-def admin_panel():
-    # Fetch all records from the database
-    librarians = User.query.filter_by(role='Librarian').all()
-    students = Student.query.all()
-    books = Book.query.all()
-    
-    # Send them to the template
-    return render_template('admin.html', librarians=librarians, students=students, books=books)
-
-@app.route('/add_librarian', methods=['POST'])
-def add_librarian():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    new_user = User(username=username, password=password, role='Librarian')
-    db.session.add(new_user)
-    db.session.commit()
-    return redirect(url_for('admin_panel'))
-
-@app.route('/add_student', methods=['POST'])
-def add_student():
-    admission_no = request.form.get('admission_no')
-    name = request.form.get('name')
-    grade = request.form.get('grade')
-    new_student = Student(admission_no=admission_no, name=name, grade=grade)
-    db.session.add(new_student)
-    db.session.commit()
-    return redirect(url_for('admin_panel'))
-
-@app.route('/add_book', methods=['POST'])
-def add_book():
-    unique_id = request.form.get('unique_book_id')
-    title = request.form.get('title')
-    author = request.form.get('author')
-    new_book = Book(unique_book_id=unique_id, title=title, author=author)
-    db.session.add(new_book)
-    db.session.commit()
-    return redirect(url_for('admin_panel'))
+def home():
+    # This just displays our front-end page
+    return render_template('admin.html')
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
